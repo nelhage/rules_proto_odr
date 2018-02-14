8ff26f7789a473b7984b5fba852eb9615e4d0d27
Nelson Elhage <nelhage@nelhage.com>
2018-02-14 10:38:32 -0800

    use the proto

--- main.cc
+++ main.cc
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "proto/with_extensions.pb.h"
+
 using namespace std;
 
 int main(int argc, char **argv) {
+    MyMessage m;
     cout << "Hello, Proto." << endl;
+    cout << m.DebugString() << endl;
     return 0;
 }
